c4e69b0011f5353868e579e38b25b2ff88d71d4e
mrudul39 <machirajumrudula39@gmail.com>
2021-07-09 23:57:31 +0530

    Adding subtotal

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
-
+import {useStateValue} from "./StateProvider";
+import { getBasketTotal } from './reducer';
 function Subtotal() {
+    const [{basket}, dispatch] = useStateValue();
     return (
         <div className="Subtotal">
         <CurrencyFormat
         renderText={(value) => (
             <>
             <p>
-       Subtotal(0 items) : <strong>0</strong>
+       Subtotal ({basket.length} items) : <strong>{value}</strong>
        
 </p>
 <small className="subtotal_gift">
@@ -19,10 +21,10 @@ function Subtotal() {
 </>
         )}
 decimalScale={2}
-value={0}
+value={getBasketTotal(basket)}
 displayType={"text"}
 thousandSeparator={true}
-prefix={"&#8377"}
+prefix={"\u20B9"}
 
 
 
