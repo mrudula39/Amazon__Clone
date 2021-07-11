7a7f1a68bad393095ff46bc00eb0524e2ce0e1bd
mrudul39 <machirajumrudula39@gmail.com>
2021-07-11 22:09:24 +0530

    adding loginn

--- src/Login.js
+++ src/Login.js
@@ -1,21 +1,67 @@
-import React from 'react'
-import './Login.css'
-import {Link} from "react-router-dom"
-
+import React from 'react';
+import "./Login.css";
+import {Link ,useHistory} from "react-router-dom";
+import { useState } from 'react';
+import {auth} from "./firebase";
 function Login() {
+const history = useHistory();
+    const [email,setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = e => {
+    e.preventDefault();
+    auth
+    .signInWithEmailAndPassword(email,password)
+    .auth(auth => {
+        history.push('/')
+    })
+    .catch(error => alert(error.message))
+
+}
+const register = e => {
+    e.preventDefault();
+
+    auth
+    .createUserWithEmailAndPassword(email, password)
+    //it sucessfully created a new user with email and password
+    .then((auth) => {
+        console.log(auth);
+        if (auth) {
+            history.push('/')
+        }
+    })
+    .catch(error => alert(error.message))
+    //do some fancy firebase register shit
+}
+
+
+
     return (
         
         <div className="login">
             <Link to ='/'>
             
-            <img className="login_logo" src="https://www.bing.com/images/search?q=amazon+logo&id=5270856F841F26C598CC717B2B7E81EE315FF84E&form=IQFRBA&first=1&disoverlay=1"/>
+            <img className="login_logo" src="https://th.bing.com/th/id/OIP.vwY-F9xvepGwJxSGNIGsgwHaEo?w=282&h=180&c=7&o=5&dpr=1.25&pid=1.7"/>
             </Link>
-            <div classNmae="login_container">
+            <div className="login_container">
                 <h1>sign-in</h1>
                 <form>
                     <h5>E-mail</h5>
-                    <input type ="text"/>
-                </form>
+                    <input type ="text"  value={email} 
+                    onChange={e => setEmail(e.target.value)}/>
+                    <h5>password</h5>
+                    <input type ="password" value={password}
+                     onChange={e => setPassword(e.target.value)} />  
+                     
+                    <button onClick={signIn}
+                    
+                    button  className="login_signInButton">Sign In</button>  
+                             </form>
+                             <p>
+                                 By signing-in you agree to AMAZON FAKE CLONE Conditions of use & sale,Please see your policy notice,our cookies notice and your Interest-Based Ads Notice.
+                             </p>
+                  <button onClick={register}
+                             >Create your Amazon Account</button>
             </div>
         </div>
     )
