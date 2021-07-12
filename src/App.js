817487046bd33fd651510687a0edef2cb4b7d912
mrudul39 <machirajumrudula39@gmail.com>
2021-07-12 23:50:04 +0530

    adding app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
- import React from 'react';
+ import React ,{ useEffect } from 'react';
 
 import './App.css';
 import Header from './Header';
@@ -6,8 +6,33 @@ import Home from "./Home";
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Checkout from "./Checkout";
 import Login from "./Login";
+import {auth } from "./firebase";
+import {useStateValue} from "./StateProvider";
 
-function App() {
+function App () {
+
+  const [{},dispatch] = useStateValue();
+  useEffect (() => {
+    //only run once app component loads
+    auth.onAuthStateChanged(authUser => {
+      console.log('THE USER IS >>>',authUser);
+
+      if (authUser) {
+        //the user just logged in / the user was logged in
+        dispatch({
+          type:'SET_USER',
+          user: authUser
+        })
+      }else{
+        //the user is logged out
+        dispatch({
+          type:'SET_USER',
+          user: null
+        })
+      
+      }
+    })
+  }, [])
   return (
     //BEM
     <BrowserRouter>
