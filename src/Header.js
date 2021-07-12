52ff0bed2f6684d36ce65fc22a2ae3e3ca53fec0
mrudul39 <machirajumrudula39@gmail.com>
2021-07-12 23:50:59 +0530

    adding heder

--- src/Header.js
+++ src/Header.js
@@ -4,8 +4,15 @@ import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import {Link} from "react-router-dom";
 import {useStateValue} from "./StateProvider"
+import { auth} from "./firebase";
 function Header() {
-    const [{basket}, dispatch] = useStateValue();
+    const [{basket, user},dispatch] = useStateValue();
+
+    const handleAuthentication = () => {
+        if(user){
+          auth.SignOut();
+        }
+    }
     return (
         <div  className='header'>
             <Link to="/"
@@ -25,10 +32,11 @@ function Header() {
             
 
         <div className="header_nav">
-            <Link to='/login'>
-            <div className='header_option'>
+            <Link to={!user && '/Login'}>
+        
+            <div  onClick ={handleAuthentication} className='header_option'>
                 <span className='header_optionLineOne'>Hello Guest</span>
-                <span className='header_optionLineTwo'>Sign In</span>
+                <span className='header_optionLineTwo'>{user ? 'Sign Out': 'Sign In'}</span>
             </div>
             </Link>
             <div className='header_option'>
