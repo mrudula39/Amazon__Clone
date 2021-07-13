fa3eec465566aa94323bd3361caa385ba8871a93
mrudul39 <machirajumrudula39@gmail.com>
2021-07-13 23:32:57 +0530

    adding file

--- src/Header.js
+++ src/Header.js
@@ -35,7 +35,7 @@ function Header() {
             <Link to={!user && '/Login'}>
         
             <div  onClick ={handleAuthentication} className='header_option'>
-                <span className='header_optionLineOne'>Hello Guest</span>
+                <span className='header_optionLineOne'>Hello {!user ? 'Guest':user.email}</span>
                 <span className='header_optionLineTwo'>{user ? 'Sign Out': 'Sign In'}</span>
             </div>
             </Link>
